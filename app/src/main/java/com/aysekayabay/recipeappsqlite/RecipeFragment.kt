87eed7d8e54aa7c7a5b9b636fea0fa2347be0330
ayse <ayseekayabay@gmail.com>
2023-08-12 23:03:17 +0300

    permissions for gallery added

--- app/src/main/java/com/aysekayabay/recipeappsqlite/RecipeFragment.kt
+++ app/src/main/java/com/aysekayabay/recipeappsqlite/RecipeFragment.kt
@@ -1,12 +1,21 @@
 package com.aysekayabay.recipeappsqlite
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class RecipeFragment : Fragment() {
+    lateinit var  saveButton : Button
+    lateinit var mealImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -19,8 +28,33 @@ class RecipeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_recipe, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        saveButton = view.findViewById(R.id.saveDetailButton)
+        mealImage = view.findViewById(R.id.addMealImageView)
+
+        saveButton.setOnClickListener {
+            saveDetail(it)
+        }
+        mealImage.setOnClickListener {
+            selectImage(it)
+        }
+    }
+
 
     fun saveDetail(view: View){
+        println("Tıklandı")
+    }
 
+    fun selectImage(view: View){
+        activity?.let {
+            if (ContextCompat.checkSelfPermission(it.applicationContext,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+            }
+            else{
+                val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent,2)
+            }
+        }
     }
 }
